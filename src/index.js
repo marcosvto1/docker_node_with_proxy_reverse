99f8b3fbd28ad52b91f751fb821430919ec281cf
Marcos <marcosvto1@gmail.com>
2021-09-06 14:30:28 -0300

    init project

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config)
+
+app.get('/', (req, res) => {
+
+  const sql = 'INSERT INTO people(name) values ("marcos")';
+  connection.query(sql)
+
+  const select = "SELECT * FROM people"
+  connection.query(select, function(err, result) {
+    if (err) {
+      res.json({
+        'msg': err
+      })
+
+      return;
+    }
+
+    const peoples = result.reduce((acc, item) => {
+      return acc + `<li>${item.name}</li>`
+    }, "")
+
+    const html = `
+      <h1>Full Cycle Rocks!</h1>
+      <ul>
+        ${peoples}
+      </ul>
+    `
+    res.send(html)
+    return;
+  })
+
+
+  return;
+
+})
+
+app.listen(5000, () => {
+  console.log('server listening in :5000')
+})
